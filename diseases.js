d22ec9f6978601644369edf5aa679173993ea9b9
CrysthelAparicio <crysthel.aparicio@unitec.edu>
2020-06-26 01:31:12 -0600

    Sistema de prediccion con tensorflow funcional

--- diseases.js
+++ diseases.js
@@ -57,18 +57,24 @@ const SINTOMAS = [
     { id: 40, name: "Rubor Facial"} //H
 ];
 
-//const CANT_DISEASES = DISEASES.length;
+const CANT_DISEASES = DISEASES.length;
 
 //data para entrenamiento
+
 const DISEASES_DATA = [
     [ 6, 7, 9, 20, 1],   [ 8, 12, 15, 7, 1],   [ 20, 9, 4, 3, 1],
     [ 34, 35, 36, 37, 1],[ 38, 39, 20, 3, 1],  [ 7, 37, 34, 9, 1],
     [ 40, 34, 39, 35, 1],[ 36, 37, 34, 40, 1], [ 8, 6, 20, 3, 1],
     [ 20, 9, 7, 6, 1],   [ 7, 15, 12, 8, 1],   [ 9, 34, 37, 7, 1],  
     [ 9, 36, 39, 6, 1],  [ 12, 39, 37, 15, 1], [ 9, 37, 20, 34, 1],
-    [ 6, 34, 40, 20 ,2], [ 8, 38, 36, 7, 1],   [ 20, 7, 8, 9, 1],
+    [ 6, 34, 40, 20 , 1], [ 8, 38, 36, 7, 1],   [ 20, 7, 8, 9, 1],
     [ 20, 37, 35, 6, 1], [ 7, 3, 40, 15, 1],   [ 9, 37, 6, 34, 1], 
- 
+    [ 1, 5, 10, 38, 1], [11, 14, 30, 34, 1], [12, 26, 29, 39, 1],
+    [ 25, 34, 36, 38, 1], [2, 3, 33, 40, 1], [7, 25, 34, 35, 1],
+    [2, 3, 31, 33, 1], [26, 36, 39, 40, 1], [29, 18, 26, 37, 1],
+    [2, 4, 10, 37, 1], [36, 37, 38, 39, 1], [9, 32, 31, 35, 1],
+    [7, 25, 34, 35, 1], [25, 33, 36, 37, 1], [8, 20, 21, 3, 1],
+
     /////
     [ 7, 8, 9,  5, 2],     [ 1, 2, 15,  5, 2],   [  5, 9, 8, 1, 2],
     [ 21, 22, 23, 24, 2],  [ 25, 26, 23, 21, 2], [ 24, 22, 25, 5, 2],
@@ -77,18 +83,34 @@ const DISEASES_DATA = [
     [ 5, 24, 22, 26, 2],   [ 2, 26, 24, 25, 2],  [ 15, 5, 1, 7, 2],
     [ 22, 9, 8, 5, 2],     [ 15, 1, 7, 24, 2],   [ 7, 26, 2, 21, 2],
     [ 1, 8, 9, 5, 2],      [ 8, 25, 1, 21, 2],   [ 26, 24, 23, 7, 2],
+    [7, 25, 34, 40, 2], [31, 32, 39, 40, 2], [30, 40, 1, 27, 2],
+    [ 29, 33, 38, 40, 2], [29, 32, 33, 38, 2], [23, 36, 37, 38, 2],
+    [7, 24, 34, 36, 2], [29, 31, 34, 40, 2], [33, 34, 36, 39, 2],
+    [24, 25, 27, 28, 2], [7, 25, 30, 39, 2], [29, 30, 14, 33, 2],
+    [32, 33, 3, 40, 2], [16, 20, 24, 36, 2], [30, 35, 37, 40, 2],
 
 
     //// Faltan aqui 
-    [ 20, 10, 19, 11, 3], [ 18, 12, 17, 13, 3], [ 16, 14, 15, 20, 3]
+    [ 20, 10, 19, 11, 3], [ 18, 12, 17, 13, 3], [ 16, 14, 15, 20, 3],
+    [7, 25, 27, 36, 3], [3, 12, 33, 39, 3], [12, 2, 13, 31, 3],
+    [30, 40, 13, 1, 3], [10, 21, 15, 27, 3], [17, 10, 17, 37, 3],
+    [31, 32, 34, 39, 3], [7, 25, 41, 37, 3], [16, 20, 27, 39, 3],
+    [12, 10, 28, 35, 3], [16, 20, 24, 36, 3], [6, 10, 22, 37, 3],
+    [29, 32, 36, 38, 3], [26, 11, 12, 28, 3], [16, 21, 22, 40, 3],
+    [35, 22, 35, 40, 3], [7, 25, 30, 39, 3], [36, 11, 16, 37, 3],
+    [16, 20, 24, 36, 3], [16, 22, 29, 40, 3], [10, 11, 13, 33, 3],
+    [11, 38, 19, 20, 3], [24, 25, 27, 28, 3], [21, 28, 26, 20, 3],
+    [7, 25, 30, 39, 3], [17, 11, 19, 30, 3], [39, 21, 14, 40, 3],
+    [29, 32, 35, 37, 3], [38, 22, 15, 47, 3], [38, 13, 25, 38, 3],
+    [28, 2, 17, 23, 3], [33, 34, 38, 39, 3], [18, 28, 26, 17, 3]
 ];
-/*
+
 function getDiseases(testSplit) {
     return tf.tidy(() => {
-        const dataByClass = [];
-        const targetByClass = [];
+        const dataByClass = []; //son los id de los sintomas
+        const targetByClass = []; // id de las enfermedades
         for (let i = 0; i < DISEASES.length; ++i) {
-            dataByClass.push([]);
+            dataByClass.push([]); //ponemos arreglos vacios dentro de los arreglos que declaramos arriba
             targetByClass.push([]);
         }
         for(const example of DISEASES_DATA){
@@ -97,12 +119,6 @@ function getDiseases(testSplit) {
             dataByClass[target-1].push(data);
             targetByClass[target-1].push(target);
         }
-        console.log(dataByClass);
-        console.log(targetByClass);
-        
-        
-        console.log(dataByClass);
-        console.log(targetByClass);
 
         const xTrains = [];
         const yTrains = [];
@@ -110,7 +126,7 @@ function getDiseases(testSplit) {
         const yTests = [];
 
         for (let i = 0; i < DISEASES.length; ++i){
-            const [xTrain, yTrain, xTest, yTest] = convertToTensors(dataByClass[i], targetByClass[i], testSplit);
+            const [xTrain, yTrain, xTest, yTest] = convertToTensors(dataByClass[i], targetByClass[i], testSplit); 
             xTrains.push(xTrain);
             yTrains.push(yTrain);
             xTests.push(xTest);
@@ -120,10 +136,36 @@ function getDiseases(testSplit) {
         const concatAxis = 0;
         return [
             tf.concat(xTrains, concatAxis), tf.concat(yTrains, concatAxis), 
-            tf.concat(xTests, concatAxis), tf.concat(yTest, concatAxis)
+            tf.concat(xTests, concatAxis), tf.concat(yTests, concatAxis)
         ];
     });
 }
 
+
+function convertToTensors(data, target, testSplit){
+    const numExamples = data.length; //cantidad total de datos
+    if ( numExamples !== target.length ) {
+        throw new Error("La cantidad de datos no coincide con la cantidad de enfermedades");
+    }
+
+    const numTestExamples = Math.round(numExamples*testSplit);//de la cantidad total de datos solo extrae un porcentaje de los mismos para brueba
+    const numTrainExamples = numExamples - numTestExamples; //los dos restante que no se usan para prueba, se utilizan para entrenamiento
+
+    const xDim = data[0].length;
+
+    //construyendo un tensor2D a partir de los datos de recopilados de una fila  
+    const xs = tf.tensor2d(data, [numExamples, xDim]);
+
+    //Crear un tensor1d y convertir a onehot encoding
+    const ys = tf.oneHot(tf.tensor1d(target).toInt(), CANT_DISEASES);
+    
+    //Dividir la y retornarla
+    const xTrain = xs.slice([0,0], [numTrainExamples, xDim]);
+    const xTest = xs.slice([numTrainExamples, 0], [numTestExamples, xDim]);
+    const yTrain = ys.slice([0,0], [numTrainExamples, CANT_DISEASES]);
+    const yTest = ys.slice([0,0], [numTestExamples, CANT_DISEASES]);
+    return [xTrain, yTrain, xTest, yTest];
+}
+
 exports.getDiseases = getDiseases;
-exports.DISEASES = DISEASES;*/
+exports.DISEASES = DISEASES;
--- tfmotor.js
+++ tfmotor.js
@@ -1,3 +1,52 @@
 const dis = require('./diseases');
+const tf = require('@tensorflow/tfjs');
 
-dis.getDiseases(.2);
+async function trainModel(xTrain, yTrain, xTest, yTest){
+    const model = tf.sequential();
+    const learningRate = .01;
+    const numberOfEpochs = 1000;
+    const optimizer = tf.train.adam(learningRate);
+
+    model.add(tf.layers.dense(
+        { units: 10, activation: 'sigmoid', inputShape: [xTrain.shape[1]]}
+    ));
+
+    model.add(tf.layers.dense(
+        { units: 3, activation: 'softmax'}
+    ));
+
+    model.compile(
+        { optimizer: optimizer, loss: 'categoricalCrossentropy', metrics: ['accuracy']}
+    );
+
+    const history = await model.fit(xTrain, yTrain, { 
+        epochs: numberOfEpochs, validationData: [xTest, yTest], callbacks: {
+            onEpochEnd: async (epoch, logs) => {
+                //console.log("Epoch: "+epoch+" Logs: "+ logs.loss);
+                await tf.nextFrame();
+            }
+        }});
+    return model;
+}
+
+async function doDiseases(){
+    const [xTrain, yTrain, xTest, yTest ] = dis.getDiseases(.2);
+    const model = await trainModel(xTrain, yTrain, xTest, yTest);
+    const input = tf.tensor2d([ 10, 11, 13, 33 ], [1, 4]);
+    const prediction = model.predict(input).argMax(-1).dataSync();
+    console.log("La enfermedad es: " + await getDiseaseName(prediction));
+}
+
+async function getDiseaseName( idPrediction ){
+    console.log("El id devuelto por la prediccion es:"+ idPrediction[0]);
+    let diseaseName = "El analisis devolvio una enfermedad que no existe!";
+    for (let i = 0; i < dis.DISEASES.length; i++) {
+        if(dis.DISEASES[i].id===idPrediction[0]){
+            diseaseName=dis.DISEASES[i].name;
+        } 
+    }
+    return diseaseName;
+}
+
+
+doDiseases();
